61dd80d18a9a37bdb4191cea20dc385e4a858e1e
lindalonglong <73254684+lindalonglong@users.noreply.github.com>
2021-01-06 17:40:22 -0700

    Create aboutus.py

---
+++ aboutus.py
@@ -0,0 +1,35 @@
+# data for the about us page
+def sophie():
+  name = "Sophie Lee"
+  image = ""
+  grade = "senior"
+  funfact = ""
+  profile = {"name": name, "image": image, "grade": grade, "funfact": funfact}
+  return profile
+
+def eva():
+  name = "Eva Gravin"
+  image = "e_aboutus.jpg"
+  grade = "junior"
+  funfact = "Eva Gravin is a junior at Del Norte High School in Computer Science Principles. She enjoys coding, going to the beach, and the fall."
+  profile = {"name": name, "image": image, "grade": grade, "funfact": funfact}
+  return profile
+
+def ali():
+  name = "Ali Saad"
+  image = "a_aboutus.jpg"
+  grade = "senior"
+  funfact = "Ali Saad is a senior at Del Norte High School in Computer Science Principles. He shares the same birthday as Travis Scott!"
+  profile = {"name": name, "image": image, "grade": grade, "funfact": funfact}
+  return profile
+
+def linda():
+  name = "Linda Long"
+  image = ""
+  grade = "senior"
+  funfact = ""
+  profile = {"name": name, "image": image, "grade": grade, "funfact": funfact}
+  return profile
+
+def about():
+  return [eva(), sophie(), ali(), linda()]
