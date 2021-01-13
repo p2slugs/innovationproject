60cc9ec5d241fc96495effba5e35cb0a7903baaa
lindalonglong <73254684+lindalonglong@users.noreply.github.com>
2021-01-13 11:03:58 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,6 @@
-
+#"app" and host need to be changed
+from main import app 
+ 
+if __name__ == "__main__":
+    # runs the application on the repl development server
+    app.run(debug=True, host='192.168.1.1', port='3000'
