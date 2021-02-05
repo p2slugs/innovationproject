aeea08e0cae7815a9a1cfe5529bbf62bcb9f236a
lindalonglong <lindal49692@gmail.com>
2021-02-05 02:08:08 -0700

    delete overlap in init.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, LoginManager
-from werkzeug.security import generate_password_hash, check_password_hash
-from control import app
 from flask import Flask
 app = Flask(__name__)
 
 dbURI = 'sqlite:///models/myDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
-app.config['SECRET_KEY'] = 'SECRET_KEY'
 db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
