c71b1da1d6622c0ffeebca8f0632501280dde880
Sophie Lee <57341391+sophieleeajh@users.noreply.github.com>
2021-03-08 09:32:01 -0800

    Update ingredients.py

--- ingredients.py
+++ ingredients.py
@@ -68,15 +68,165 @@ def spicy_pepper():
     profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
     return profile
 
-def big_hearty_radish():
+def voltfruit():
     box = "box8"
     box2 = "hdg"
-    ingredient = "big hearty radish"
-    category = "vegetable"
-    hearts = "4"
-    price = "2"
+    ingredient = "voltfruit"
+    category = "fruit"
+    hearts = "1/2"
+    price = "4"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def fleet_lotus_seeds():
+    box = "box9"
+    box2 = "hdg"
+    ingredient = "fleet-lotus seeds"
+    category = "fruit"
+    hearts = "1/2"
+    price = "5"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def mighty_bananas():
+    box = "box10"
+    box2 = "hdg"
+    ingredient = "mighty bananas"
+    category = "fruit"
+    hearts = "1/2"
+    price = "5"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def big_hearty_truffle():
+    box = "box11"
+    box2 = "hdg"
+    ingredient = "big hearty truffle"
+    category = "mushroom"
+    hearts = "3"
+    price = "15"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def hearty_truffle():
+    box = "box12"
+    box2 = "hdg"
+    ingredient = "hearty truffle"
+    category = "mushroom"
+    hearts = "2"
+    price = "6"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def endura_shroom():
+    box = "box13"
+    box2 = "hdg"
+    ingredient = "endura shroom"
+    category = "mushroom"
+    hearts = "1"
+    price = "6"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def hylian_shroom():
+    box = "box14"
+    box2 = "hdg"
+    ingredient = "hylian shroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def stamella_shroom():
+    box = "box15"
+    box2 = "hdg"
+    ingredient = "stamella shroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "5"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def chillshroom():
+    box = "box15"
+    box2 = "hdg"
+    ingredient = "chillshroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "4"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def sunshroom():
+    box = "box16"
+    box2 = "hdg"
+    ingredient = "sunshroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "4"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def zapshroom():
+    box = "box17"
+    box2 = "hdg"
+    ingredient = "zapshroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "4"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def rushroom():
+    box = "box18"
+    box2 = "hdg"
+    ingredient = "rushshroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def razorshroom():
+    box = "box19"
+    box2 = "hdg"
+    ingredient = "razorshroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "5"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def ironshroom():
+    box = "box20"
+    box2 = "hdg"
+    ingredient = "ironshroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "5"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def silent_shroom():
+    box = "box21"
+    box2 = "hdg"
+    ingredient = "silent shroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def ironshroom():
+    box = "box20"
+    box2 = "hdg"
+    ingredient = "ironshroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "5"
     profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
     return profile
 
 def ingredients():
-    return [hearty_durian(), palm_fruit(), apple(), wildberry(), hydromelon(), spicy_pepper(), voltfruit(), big_hearty_radish()]
+    return [hearty_durian(), palm_fruit(), apple(), wildberry(), hydromelon(), spicy_pepper(), voltfruit(), fleet_lotus_seeds(), mighty_bananas(), big_hearty_truffle(), hearty_truffle(), endura_shroom(), hylian_shroom(), stamella_shroom(), chillshroom(), sunshroom(), zapshroom(), rushroom(), razorshroom(), ironshroom()]
