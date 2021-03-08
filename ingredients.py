5c48e91b0d6fc1789599566d7a5bd172b4123968
Sophie Lee <57341391+sophieleeajh@users.noreply.github.com>
2021-03-08 14:25:53 -0800

    Update ingredients.py

--- ingredients.py
+++ ingredients.py
@@ -219,7 +219,7 @@ def silent_shroom():
     return profile
 
 def ironshroom():
-    box = "box20"
+    box = "box21"
     box2 = "hdg"
     ingredient = "ironshroom"
     category = "mushroom"
@@ -228,5 +228,335 @@ def ironshroom():
     profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
     return profile
 
+def silent_shroom():
+    box = "box22"
+    box2 = "hdg"
+    ingredient = "silent shroom"
+    category = "mushroom"
+    hearts = "1/2"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def big_hearty_radish():
+    box = "box23"
+    box2 = "hdg"
+    ingredient = "big hearty radish"
+    category = "vegetable"
+    hearts = "4"
+    price = "15"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def hearty_radish():
+    box = "box24"
+    box2 = "hdg"
+    ingredient = "hearty radish"
+    category = "vegetable"
+    hearts = "2.5"
+    price = "8"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def endura_carrot():
+    box = "box25"
+    box2 = "hdg"
+    ingredient = "endura carrot"
+    category = "vegetable"
+    hearts = "2"
+    price = "30"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def hyrule_herb():
+    box = "box26"
+    box2 = "hdg"
+    ingredient = "hyrule herb"
+    category = "flower/herb"
+    hearts = "1"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def swift_carrot():
+    box = "box27"
+    box2 = "hdg"
+    ingredient = "swift carrot"
+    category = "vegetable"
+    hearts = "1/2"
+    price = "4"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def fortified_pumpkin():
+    box = "box28"
+    box2 = "hdg"
+    ingredient = "fortified pumpkin"
+    category = "vegetable"
+    hearts = "1/2"
+    price = "4"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def cool_safflina():
+    box = "box29"
+    box2 = "hdg"
+    ingredient = "cool safflina"
+    category = "flower/herb"
+    hearts = "0"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def warm_safflina():
+    box = "box30"
+    box2 = "hdg"
+    ingredient = "warm safflina"
+    category = "flower/herb"
+    hearts = "0"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def electric_safflina():
+    box = "box31"
+    box2 = "hdg"
+    ingredient = "electric safflina"
+    category = "flower/herb"
+    hearts = "0"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def swift_violet():
+    box = "box32"
+    box2 = "hdg"
+    ingredient = "swift violet"
+    category = "flower/herb"
+    hearts = "0"
+    price = "10"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def mighty_thistle():
+    box = "box33"
+    box2 = "hdg"
+    ingredient = "mighty thistle"
+    category = "flower/herb"
+    hearts = "0"
+    price = "5"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def armoranth():
+    box = "box34"
+    box2 = "hdg"
+    ingredient = "armoranth"
+    category = "flower/herb"
+    hearts = "0"
+    price = "5"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def blue_nightshade():
+    box = "box35"
+    box2 = "hdg"
+    ingredient = "blue nightshade"
+    category = "flower/herb"
+    hearts = "0"
+    price = "4"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def silent_princess():
+    box = "box36"
+    box2 = "hdg"
+    ingredient = "silent princess"
+    category = "flower/herb"
+    hearts = "0"
+    price = "10"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def raw_gourmet_meat():
+    box = "box37"
+    box2 = "hdg"
+    ingredient = "raw gourmet meat"
+    category = "meat"
+    hearts = "3"
+    price = "35"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def raw_whole_bird():
+    box = "box38"
+    box2 = "hdg"
+    ingredient = "raw whole bird"
+    category = "meat"
+    hearts = "3"
+    price = "35"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def raw_prime_meat():
+    box = "box39"
+    box2 = "hdg"
+    ingredient = "raw prime meat"
+    category = "meat"
+    hearts = "1.5"
+    price = "15"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def raw_bird_thigh():
+    box = "box40"
+    box2 = "hdg"
+    ingredient = "raw bird thigh"
+    category = "meat"
+    hearts = "1.5"
+    price = "15"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def raw_meat():
+    box = "box41"
+    box2 = "hdg"
+    ingredient = "raw meat"
+    category = "meat"
+    hearts = "1"
+    price = "8"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def raw_bird_drumstick():
+    box = "box42"
+    box2 = "hdg"
+    ingredient = "raw bird drumstick"
+    category = "meat"
+    hearts = "1"
+    price = "8"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def courser_bee_honey():
+    box = "box43"
+    box2 = "hdg"
+    ingredient = "courser bee honey"
+    category = "other"
+    hearts = "2"
+    price = "10"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def hylian_rice():
+    box = "box44"
+    box2 = "hdg"
+    ingredient = "hylian rice"
+    category = "other"
+    hearts = "1"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def bird_egg():
+    box = "box45"
+    box2 = "hdg"
+    ingredient = "bird egg"
+    category = "other"
+    hearts = "1"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def tabantha_wheat():
+    box = "box46"
+    box2 = "hdg"
+    ingredient = "tabantha wheat"
+    category = "other"
+    hearts = "1"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def fresh_milk():
+    box = "box47"
+    box2 = "hdg"
+    ingredient = "fresh milk"
+    category = "other"
+    hearts = "1/2"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def acorn():
+    box = "box48"
+    box2 = "hdg"
+    ingredient = "acorn"
+    category = "other"
+    hearts = "1/4"
+    price = "2"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def chickaloo_tree_nut():
+    box = "box49"
+    box2 = "hdg"
+    ingredient = "chickaloo tree nut"
+    category = "other"
+    hearts = "1/4"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def cane_sugar ():
+    box = "box50"
+    box2 = "hdg"
+    ingredient = "cane sugar"
+    category = "other"
+    hearts = "0"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def goat_butter():
+    box = "box51"
+    box2 = "hdg"
+    ingredient = "goat butter"
+    category = "other"
+    hearts = "0"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def goron_spice():
+    box = "box52"
+    box2 = "hdg"
+    ingredient = "goron spice"
+    category = "other"
+    hearts = "0"
+    price = "4"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def rock_salt():
+    box = "box53"
+    box2 = "hdg"
+    ingredient = "rock salt"
+    category = "other"
+    hearts = "0"
+    price = "2"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
+def monster_extract():
+    box = "box54"
+    box2 = "hdg"
+    ingredient = "monster extract"
+    category = "other"
+    hearts = "0"
+    price = "3"
+    profile = {"box": box, "box2": box2, "category": category, "ingredient": ingredient, "hearts": hearts, "price": price}
+    return profile
+
 def ingredients():
-    return [hearty_durian(), palm_fruit(), apple(), wildberry(), hydromelon(), spicy_pepper(), voltfruit(), fleet_lotus_seeds(), mighty_bananas(), big_hearty_truffle(), hearty_truffle(), endura_shroom(), hylian_shroom(), stamella_shroom(), chillshroom(), sunshroom(), zapshroom(), rushroom(), razorshroom(), ironshroom()]
+    return [hearty_durian(), palm_fruit(), apple(), wildberry(), hydromelon(), spicy_pepper(), voltfruit(), fleet_lotus_seeds(), mighty_bananas(), big_hearty_truffle(), hearty_truffle(), endura_shroom(), hylian_shroom(), stamella_shroom(), chillshroom(), sunshroom(), zapshroom(), rushroom(), razorshroom(), ironshroom(), silent_shroom(), big_hearty_radish(), hearty_radish(), endura_carrot(), hyrule_herb(), swift_carrot(), fortified_pumpkin(), cool_safflina(), warm_safflina(), electric_safflina(), swift_violet(), mighty_thistle(), armoranth(), blue_nightshade(), silent_princess(), raw_gourmet_meat(), raw_whole_bird(), raw_prime_meat(), raw_bird_thigh(), raw_meat(), raw_bird_drumstick(), courser_bee_honey(), hylian_rice(), bird_egg(), tabantha_wheat(), fresh_milk(), acorn(), chickaloo_tree_nut(), cane_sugar(), goat_butter(), goron_spice(), rock_salt(), monster_extract()]
